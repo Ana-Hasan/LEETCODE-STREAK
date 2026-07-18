fb424b77c0b7cbe77741c25be6d49abfb3e2cfe9
Ana Hasan <ana.github.hasan@gmail.com>
2026-07-18 12:46:07 +0530

    Time: 67 ms (37.02%), Space: 111.3 MB (23.02%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-
-        for (int i = 1; i < nums.size(); i++) {
-            if (nums[i] == nums[i - 1]) {
+        unordered_set<int> seen;
+        for(int i = 0; i <= nums.size() - 1; i++){
+            if(seen.find(nums[i]) != seen.end()){
                 return true;
             }
+            else{
+                seen.insert(nums[i]);
+            }
         }
-
         return false;
     }
 };
